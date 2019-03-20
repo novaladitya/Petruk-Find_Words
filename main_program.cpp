a5af78ea1de529bf0b620b7bb8ce8d25677e33e6
mirfan11 <47858424+mirfan11@users.noreply.github.com>
2019-03-20 23:17:51 +0700

    Update main_program.cpp
    add vertikal searching bolak balik

--- main_program.cpp
+++ main_program.cpp
@@ -77,6 +77,37 @@ int mencariKata(char *kata){
 			else
 				simpanHasil;
 			simpan = cek;
+			//vertikal Atas-Bawah
+			for(int k=0; k<strlen(kata); k++){
+				if(kata[k]==*(*(puzzle+(i+k))+j)){
+					simpan = k;
+				}
+				else{
+					simpan != k;
+					break;
+				}
+			}
+			if(simpan==(strlen(kata)-1))
+				simpanHasil = simpanHasil+1;
+			else
+				simpanHasil;
+			simpan = cek;
+
+			//vertikal Bawah-Atas
+			for(int k=0; k<strlen(kata); k++){
+				if(kata[k]==*(*(puzzle+(i-k))+j)){
+					simpan = k;
+				}
+				else{
+					simpan != k;
+					break;
+				}
+			}
+			if(simpan==(strlen(kata)-1))
+				simpanHasil = simpanHasil+1;
+			else
+				simpanHasil;
+			simpan = cek;
 		}
 	}
 }
