062a63247e8a3d60c6f61df5e643afef963f0ddb
mirfan11 <47858424+mirfan11@users.noreply.github.com>
2019-03-20 22:47:25 +0700

    Menambah komentar

--- Searching_Vertikal.cpp
+++ Searching_Vertikal.cpp
@@ -1,30 +1,31 @@
+//vertikal Atas-Bawah
 for(int k=0; k<strlen(kata); k++){
-				if(kata[k]==*(*(puzzle+(i+k))+j)){
-					simpan = k;
-				}
-				else{
-					simpan != k;
-					break;
-				}
-			}
-			if(simpan==(strlen(kata)-1))
-				simpanHasil = simpanHasil+1;
-			else
-				simpanHasil;
-			simpan = cek;
-			
-			//vertikal bawah-atas
-			for(int k=0; k<strlen(kata); k++){
-				if(kata[k]==*(*(puzzle+(i-k))+j)){
-					simpan = k;
-				}
-				else{
-					simpan != k;
-					break;
-				}
-			}
-			if(simpan==(strlen(kata)-1))
-				simpanHasil = simpanHasil+1;
-			else
-				simpanHasil;
-			simpan = cek;
+	if(kata[k]==*(*(puzzle+(i+k))+j)){
+		simpan = k;
+	}
+	else{
+		simpan != k;
+		break;
+	}
+}
+if(simpan==(strlen(kata)-1))
+	simpanHasil = simpanHasil+1;
+else
+	simpanHasil;
+simpan = cek;
+
+//vertikal Bawah-Atas
+for(int k=0; k<strlen(kata); k++){
+	if(kata[k]==*(*(puzzle+(i-k))+j)){
+		simpan = k;
+	}
+	else{
+		simpan != k;
+		break;
+	}
+}
+if(simpan==(strlen(kata)-1))
+	simpanHasil = simpanHasil+1;
+else
+	simpanHasil;
+simpan = cek;
