ecf8a96e175e848d84ae4c498d61a42524969003
Azrielbs04 <48719242+Azrielbs04@users.noreply.github.com>
2019-03-20 23:21:26 +0700

    Update main_program.cpp
    menambah searching diagonal kiri atas  - kanan bawah dan kanan bawah - kiri atas

--- main_program.cpp
+++ main_program.cpp
@@ -108,6 +108,38 @@ int mencariKata(char *kata){
 			else
 				simpanHasil;
 			simpan = cek;
+			
+			//diagonal kiri atas-kanan bawah
+			for(int k=0; k<strlen(kata); k++){
+				if(kata[k]==*(*(puzzle+(i+k))+(j+k))){
+					simpan = k;
+				}
+				else{
+					simpan != k;
+					break;
+				}
+			}
+			if(simpan==(strlen(kata)-1))
+				simpanHasil = simpanHasil+1;
+			else
+				simpanHasil;
+			simpan = cek;
+			
+			//diagonal kanan bawah-kiri atas
+			for(int k=0; k<strlen(kata); k++){
+				if(kata[k]==*(*(puzzle+(i-k))+(j-k))){
+					simpan = k;
+				}
+				else{
+					simpan != k;
+					break;
+				}
+			}
+			if(simpan==(strlen(kata)-1))
+				simpanHasil = simpanHasil+1;
+			else
+				simpanHasil;
+			simpan = cek;
 		}
 	}
 }
